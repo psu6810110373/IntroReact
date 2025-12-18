e8f1e4029eb5ff7fe34b658d638dbd8135d438b6
Sittinon Dormae <6810110373@psu.ac.th>
2025-12-18 16:05:11 +0700

    2nd_LoginPages

--- src/App.jsx
+++ src/App.jsx
@@ -3,24 +3,33 @@ import axios from 'axios'
 import { useState } from 'react';
 import LoginScreen from './LoginScreen';
 import BookScreen from './BookScreen';
+import { Routes, Route, Navigate } from 'react-router-dom';
 
 axios.defaults.baseURL = "http://localhost:3000"
 
 function App() {
   const [isAuthenticated, setIsAuthenticated] = useState(false)
+  
   const handleLoginSuccess = (token) => {
     axios.defaults.headers.common = { 'Authorization': `Bearer ${token}` }
-    
     setIsAuthenticated(true)
   }
 
   return (
-    <>
-      {isAuthenticated ? 
-        <BookScreen/> : 
-        <LoginScreen onLoginSuccess={handleLoginSuccess}/>
-      }
-    </>
+   <Routes>
+      <Route 
+        path="/login" 
+        element={
+          isAuthenticated ? <Navigate to="/" /> : <LoginScreen onLoginSuccess={handleLoginSuccess}/>
+        } 
+      />
+      <Route 
+        path="/" 
+        element={
+          isAuthenticated ? <BookScreen/> : <Navigate to="/login" />
+        } 
+      />
+    </Routes>
   )
 }
 
